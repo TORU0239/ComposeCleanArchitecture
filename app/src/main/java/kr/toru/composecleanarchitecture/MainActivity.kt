5eef969f1607392cfc0804fb01bdcc1d02db1972
Toru Choi <blueocean239@gmail.com>
2024-06-18 17:08:30 +0900

    feat: test widget

--- app/src/main/java/kr/toru/composecleanarchitecture/MainActivity.kt
+++ app/src/main/java/kr/toru/composecleanarchitecture/MainActivity.kt
@@ -1,40 +1,86 @@
 package kr.toru.composecleanarchitecture
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kr.toru.composecleanarchitecture.ui.theme.ComposeCleanArchitectureTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ComposeCleanArchitectureTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    text = "TopAppBar!!!",
+                                )
+                            }
+                        )
+                    },
+                    content = { innerPadding ->
+                        Column(modifier = Modifier.padding(innerPadding)) {
+                            Spacer(modifier = Modifier
+                                .height(10.dp)
+                            )
+                            Greeting(
+                                name = "Android",
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .wrapContentSize()
+                            )
+                            Spacer(modifier = Modifier
+                                .height(10.dp)
+                            )
+                            ScrollBoxes()
+                        }
+                    },
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+private fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.background(Color.Red)
     )
 }
 
@@ -43,5 +89,24 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     ComposeCleanArchitectureTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+private fun ScrollBoxes() {
+    Column(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        repeat(100) {
+            Text(
+                text = "Item $it",
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(16.dp)
+            )
+        }
     }
 }
--- app/src/main/java/kr/toru/composecleanarchitecture/ui/theme/Theme.kt
+++ app/src/main/java/kr/toru/composecleanarchitecture/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package kr.toru.composecleanarchitecture.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
